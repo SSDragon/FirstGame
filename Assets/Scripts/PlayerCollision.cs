5a574953823e8fd8653542d4da5ce9b3babb0e82
Tamás <kovacst@vartid.com>
2021-04-28 16:06:25 +0200

    base game files created

---
+++ Assets/Scripts/PlayerCollision.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour
+{
+    public PlayerMovement movement;
+
+    void OnCollisionEnter(Collision collosionInfo)
+    {
+        if(collosionInfo.collider.tag == "Obstacle")
+        {
+            movement.enabled = false;
+        }
+    }
+
+
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody rb;
+
+    public float forwardForce = 2000f;
+    public float sidewaysForce = 500f;
+
+
+    void Start()
+    {
+
+    }
+
+    void FixedUpdate()
+    {
+        //add a forward force
+        rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+
+        if(Input.GetKey("d"))
+        {
+            rb.AddForce(sidewaysForce, 0, 0 * Time.deltaTime);
+        }
+
+        if(Input.GetKey("a")) {
+            rb.AddForce(-sidewaysForce, 0, 0 * Time.deltaTime);
+        }
+
+    }
+}
