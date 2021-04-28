5a76fb1c5f6cba9e89449f8bd9dd7b60f6f9541f
Tamás <kovacst@vartid.com>
2021-04-28 16:54:19 +0200

    movements changed

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -20,11 +20,11 @@ public class PlayerMovement : MonoBehaviour
 
         if(Input.GetKey("d"))
         {
-            rb.AddForce(sidewaysForce, 0, 0 * Time.deltaTime);
+            rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
         }
 
         if(Input.GetKey("a")) {
-            rb.AddForce(-sidewaysForce, 0, 0 * Time.deltaTime);
+            rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
         }
 
     }
